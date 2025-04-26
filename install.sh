6c51b484d3844895bc6103f75bc64c3334624d44
Eugene Stahov <eugene.stahov@gmail.com>
2025-04-26 16:14:01 +0500

    make some path if KlipperScreen is not installed yet

--- install.sh
+++ install.sh
@@ -8,10 +8,12 @@ THEME="z-bolt3.5"
 TARGET=$HOME/KlipperScreen/styles
 
 sudo service KlipperScreen stop
+mkdir -p $TARGET
 rm -rf $TARGET/$THEME
 cp -r $SPATH/$THEME "$TARGET/"
 sudo cp $SPATH/$THEME/settings.ini /etc/gtk-3.0/
 
+mkdir -p ~/printer_data/KlipperScreen
 sed -i '/theme =.*/d' /tmp/KlipperScreen.conf
 cat KlipperScreen.conf.theme_snippet ~/printer_data/KlipperScreen.conf > /tmp/KlipperScreen.conf
 mv /tmp/KlipperScreen.conf ~/printer_data/
