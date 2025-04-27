77bf1984f24e1c5029ad00453e2a11ba0884e536
Eugene Stahov <eugene.stahov@gmail.com>
2025-04-27 22:53:28 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,17 +7,22 @@ THEME="z-bolt3.5"
 
 TARGET=$HOME/KlipperScreen/styles
 
+KLIPPERSCREENCONF=$HOME/printer_data/config/KlipperScreen.conf
+
 sudo service KlipperScreen stop
 mkdir -p $TARGET
 rm -rf $TARGET/$THEME
 cp -r $SPATH/$THEME "$TARGET/"
 sudo cp $SPATH/$THEME/settings.ini /etc/gtk-3.0/
 
-## FIXME
-# sed -i '/theme =.*/d' ~/printer_data/config/KlipperScreen.conf
-# cat $SPATH/KlipperScreen.conf.theme_snippet ~/printer_data/config/KlipperScreen.conf > /tmp/KlipperScreen.conf
-# mv /tmp/KlipperScreen.conf ~/printer_data/config
-
+# 0 Create if not exist
+touch $KLIPPERSCREENCONF
+# 1. delete all previous theme selections
+sed -i '/theme =.*/d' $KLIPPERSCREENCONF
+# 2. Create [main] section if not exist
+grep "^\[main\]" $KLIPPERSCREENCONF || gawk -i inplace 'BEGINFILE{print"[main]"} 1' $KLIPPERSCREENCONF
+# 3. Append theme string after [main]
+sed -i "/^\[main\]/a\theme = z-bolt3.5" $KLIPPERSCREENCONF
 
 sudo service KlipperScreen start
 
