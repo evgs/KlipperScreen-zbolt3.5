af40e5143974244a7767876f75f69acc3758a8eb
Eugene Stahov <eugene.stahov@gmail.com>
2025-04-27 22:59:22 +0500

    ask if theme should be enabled

--- install.sh
+++ install.sh
@@ -15,14 +15,20 @@ rm -rf $TARGET/$THEME
 cp -r $SPATH/$THEME "$TARGET/"
 sudo cp $SPATH/$THEME/settings.ini /etc/gtk-3.0/
 
-# 0 Create if not exist
-touch $KLIPPERSCREENCONF
-# 1. delete all previous theme selections
-sed -i '/theme =.*/d' $KLIPPERSCREENCONF
-# 2. Create [main] section if not exist
-grep "^\[main\]" $KLIPPERSCREENCONF || gawk -i inplace 'BEGINFILE{print"[main]"} 1' $KLIPPERSCREENCONF
-# 3. Append theme string after [main]
-sed -i "/^\[main\]/a\theme = z-bolt3.5" $KLIPPERSCREENCONF
+
+read -p "Enable this theme (Y/N)? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # 0 Create if not exist
+  touch $KLIPPERSCREENCONF
+  # 1. delete all previous theme selections
+  sed -i '/theme =.*/d' $KLIPPERSCREENCONF
+  # 2. Create [main] section if not exist
+  grep "^\[main\]" $KLIPPERSCREENCONF || gawk -i inplace 'BEGINFILE{print"[main]"} 1' $KLIPPERSCREENCONF
+  # 3. Append theme string after [main]
+  sed -i "/^\[main\]/a\theme = z-bolt3.5" $KLIPPERSCREENCONF
+fi
 
 sudo service KlipperScreen start
 
